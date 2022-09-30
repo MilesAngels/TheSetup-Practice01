6f75151641f638a9be06fb1a58bf4c8677e9469a
MilesAngels <mangels@alphaworks.tech>
2022-09-30 08:42:27 -0700

    initial push

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<?php 
+    /**
+     * Template Name: Home
+     * 
+     * 
+     * @package TheSetup-Practice01
+     * 
+     * 
+    */
+?>
+
+<?php get_header(); ?>
+
+    <!-- Code Goes Here -->
+
+<?php get_footer(); ?>
